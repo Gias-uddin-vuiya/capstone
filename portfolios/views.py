7bb1e2c8e411d05a1d2ed93c1117348461303b73
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-12-08 13:02:08 +0600

    style: add favicon

--- portfolios/views.py
+++ portfolios/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "makes_portfolio/layout.html", {
+    return render(request, "makers_portfolio/index.html", {
         
     })
