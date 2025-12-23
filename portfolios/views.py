d6a554d38d214ab10dfb683707ea1f124ca5b139
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-12-23 12:46:18 +0600

    feat: add database to the project and make profile models for home page

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 
 def index(request):
+    profile = Profile.objects.all()  # resive all profile objects
+    
     return render(request, "makers_portfolio/index.html", {
-        
+        "profile": profile[0] if profile else None
     })
 
 
